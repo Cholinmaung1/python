8619dac669fbf9fff60ff51a51ab8c361b28e00b
Cho Lin Maung <34511025+Cholinmaung1@users.noreply.github.com>
2024-08-14 14:47:07 +0630

    Create napalm_simple_config.py

---
+++ napalm_simple_config.py
@@ -0,0 +1,8 @@
+from napalm import get_network_driver
+
+driver = get_network_driver("ios")
+iosvl2 = driver("192.168.245.129", "cisco", "cisco123")
+iosvl2.open()
+
+ios_output = iosvl2.get_facts()
+print (ios_output)
