71eed76ef4ab8efa7112cc6b35ec40a4ea3cd7c8
Cho Lin Maung <34511025+Cholinmaung1@users.noreply.github.com>
2024-08-14 15:18:07 +0630

    Update napalm_simple_config.py

--- napalm_simple_config.py
+++ napalm_simple_config.py
@@ -1,3 +1,4 @@
+import json
 from napalm import get_network_driver
 
 driver = get_network_driver("ios")
@@ -7,11 +8,17 @@ iosvl2.open()
 ios_output = iosvl2.get_facts()
 print (ios_output)
 
-#print output with json format
-print (json.dumps(ios_output, indent=4))
-
-ios_output = isovl2.get_interfaces()
-print (json.dumps(ios_output, indent=4))
+ios_output = iosvl2.get_interfaces()
+print (json.dumps(ios_output, sort_keys=True, indent=4))
 
 ios_output = iosvl2.get_interfaces_counters()
+print (json.dumps(ios_output, sort_keys=True, indent=4))
+
+ios_output = iosvl2.get_mac_address_table()
+print (json.dumps(ios_output, sort_keys=True, indent=4))
+
+ios_output = iosvl2.get_arp_table()
+print (json.dumps(ios_output, sort_keys=True, indent=4))
+
+ios_output = iosvl2.ping("8.8.8.8")
 print (json.dumps(ios_output, indent=4))
